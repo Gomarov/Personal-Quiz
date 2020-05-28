ca535fa203aeee206745eb4ea851b2811771e906
Pavel Gomarov <p.gomarov@icloud.com>
2020-05-28 22:58:52 +0300

    version 1.1

---
+++ Personal Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personal Quiz
+//
+//  Created by Павел on 28.05.2020.
+//  Copyright © 2020 Павел. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
